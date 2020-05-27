35d1d3a22ec5d3a73e646b0975de53fa44dcac55
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2020-05-26 20:20:54 -0500

    Total JS

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,4 @@
 import { elements } from './base';
-import apiShutterstock from '../config/ShutterstockConfig';
 
 const year = new Date().getFullYear();
 elements.yearNow.innerText = year;
@@ -7,18 +6,3 @@ elements.yearNow.innerText = year;
 export function getFirtsLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
-
-// const queryParams = {
-//   query: 'New York',
-//   sort: 'popular',
-//   orientation: 'horizontal',
-// };
-
-// apiShutterstock
-//   .searchImages(queryParams)
-//   .then(function ({ data }) {
-//     console.log(data);
-//   })
-//   .catch(function (error) {
-//     console.error(error);
-//   });
